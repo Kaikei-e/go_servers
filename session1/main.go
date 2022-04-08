58b7e4acbeb7c8b957726698246271a2e12e6a4b
Kaikei-e <e_kaikei@protonmail.com>
2022-04-08 23:20:39 +0900

    Update: implemented first simple tcp server

--- session1/main.go
+++ session1/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net"
+)
 
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		io.WriteString(conn, "\nHello from tcp")
+		fmt.Fprintln(conn, "How is your day?")
+		fmt.Fprintf(conn, "%v", "Well, I hope!")
+
+		conn.Close()
+	}
 }
